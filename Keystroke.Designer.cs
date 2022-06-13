f95e506d017b9b4bcc392e28186bd74df164a7ab
Myma <63861212+MymaQc@users.noreply.github.com>
2022-06-13 13:11:08 -0400

    Ajoutez des fichiers projet.

---
+++ Keystroke.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace KeystrokeMod
+{
+    partial class Keystroke
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlKeystroke = new System.Windows.Forms.Panel();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnSpace = new System.Windows.Forms.Button();
+            this.btnLeftClick = new System.Windows.Forms.Button();
+            this.btnRightClick = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.pnlKeystroke.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlKeystroke
+            // 
+            this.pnlKeystroke.BackColor = System.Drawing.Color.Wheat;
+            this.pnlKeystroke.Controls.Add(this.btnForward);
+            this.pnlKeystroke.Controls.Add(this.btnRightClick);
+            this.pnlKeystroke.Controls.Add(this.btnLeftClick);
+            this.pnlKeystroke.Controls.Add(this.btnSpace);
+            this.pnlKeystroke.Controls.Add(this.btnLeft);
+            this.pnlKeystroke.Controls.Add(this.btnRight);
+            this.pnlKeystroke.Controls.Add(this.btnBackward);
+            this.pnlKeystroke.Location = new System.Drawing.Point(13, 13);
+            this.pnlKeystroke.Name = "pnlKeystroke";
+            this.pnlKeystroke.Size = new System.Drawing.Size(200, 274);
+            this.pnlKeystroke.TabIndex = 0;
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.BackColor = System.Drawing.Color.White;
+            this.btnBackward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBackward.Font = new System.Drawing.Font("Minecraft", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackward.Location = new System.Drawing.Point(72, 134);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(55, 55);
+            this.btnBackward.TabIndex = 0;
+            this.btnBackward.Text = "S";
+            this.btnBackward.UseVisualStyleBackColor = false;
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackColor = System.Drawing.Color.White;
+            this.btnRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRight.Font = new System.Drawing.Font("Minecraft", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.Location = new System.Drawing.Point(133, 134);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(55, 55);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = "D";
+            this.btnRight.UseVisualStyleBackColor = false;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackColor = System.Drawing.Color.White;
+            this.btnLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeft.Font = new System.Drawing.Font("Minecraft", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.Location = new System.Drawing.Point(11, 134);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(55, 55);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "A";
+            this.btnLeft.UseVisualStyleBackColor = false;
+            // 
+            // btnSpace
+            // 
+            this.btnSpace.BackColor = System.Drawing.Color.White;
+            this.btnSpace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSpace.Location = new System.Drawing.Point(11, 195);
+            this.btnSpace.Name = "btnSpace";
+            this.btnSpace.Size = new System.Drawing.Size(177, 36);
+            this.btnSpace.TabIndex = 3;
+            this.btnSpace.Text = "__________________________";
+            this.btnSpace.UseVisualStyleBackColor = false;
+            // 
+            // btnLeftClick
+            // 
+            this.btnLeftClick.BackColor = System.Drawing.Color.White;
+            this.btnLeftClick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeftClick.Font = new System.Drawing.Font("Minecraft", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeftClick.Location = new System.Drawing.Point(10, 237);
+            this.btnLeftClick.Name = "btnLeftClick";
+            this.btnLeftClick.Size = new System.Drawing.Size(86, 27);
+            this.btnLeftClick.TabIndex = 4;
+            this.btnLeftClick.Text = "Gauche";
+            this.btnLeftClick.UseVisualStyleBackColor = false;
+            // 
+            // btnRightClick
+            // 
+            this.btnRightClick.BackColor = System.Drawing.Color.White;
+            this.btnRightClick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRightClick.Font = new System.Drawing.Font("Minecraft", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRightClick.Location = new System.Drawing.Point(102, 237);
+            this.btnRightClick.Name = "btnRightClick";
+            this.btnRightClick.Size = new System.Drawing.Size(86, 27);
+            this.btnRightClick.TabIndex = 5;
+            this.btnRightClick.Text = "Droite";
+            this.btnRightClick.UseVisualStyleBackColor = false;
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackColor = System.Drawing.Color.White;
+            this.btnForward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForward.Font = new System.Drawing.Font("Minecraft", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForward.Location = new System.Drawing.Point(72, 73);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(55, 55);
+            this.btnForward.TabIndex = 6;
+            this.btnForward.Text = "W";
+            this.btnForward.UseVisualStyleBackColor = false;
+            // 
+            // Keystroke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlKeystroke);
+            this.Name = "Keystroke";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Keystroke_Load);
+            this.pnlKeystroke.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlKeystroke;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnRightClick;
+        private System.Windows.Forms.Button btnLeftClick;
+        private System.Windows.Forms.Button btnSpace;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnBackward;
+    }
+}
+
---
+++ Keystroke.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeystrokeMod {
+
+    public partial class Keystroke : Form {
+
+        private static char chForward = 'W';
+        private static char chLeft = 'A';
+        private static char chBackward = 'S';
+        private static char chRight = 'D';
+
+        public static new IntPtr Handle;
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(Keys key);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT IpRect);
+
+        public static RECT rect;
+
+        public struct RECT {
+            public int left, top, right, bottom;
+        }
+
+        public Keystroke() {
+            InitializeComponent();
+        }
+
+        private void Keystroke_Load(object sender, EventArgs e) {
+            ActiveControl = pnlKeystroke;
+
+            btnForward.Text = chForward.ToString();
+            btnLeft.Text = chLeft.ToString();
+            btnBackward.Text = chBackward.ToString();
+            btnRight.Text = chRight.ToString();
+
+            CheckForIllegalCrossThreadCalls = false;
+            SetInvisible(this);
+
+            Thread thKeystroke = new Thread(() => KeystrokeMod(1, "Minecraft", this)) { IsBackground = true };
+            thKeystroke.Start();
+        }
+
+        private void SetInvisible(Form form) {
+            form.BackColor = Color.Wheat;
+            form.TransparencyKey = Color.Wheat;
+            form.TopMost = true;
+            form.FormBorderStyle = FormBorderStyle.None;
+
+            int initialSize = GetWindowLong(Handle, -20);
+            SetWindowLong(Handle, -20, initialSize | 0x8000 | 0x20);
+        }
+
+        private void KeystrokeMod(int frequency, string windowName, Form form) {
+            while (true) {
+                Handle = FindWindow(null, windowName);
+                GetWindowRect(Handle, out rect);
+                form.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
+                form.Left = rect.left;
+                form.Top = rect.top;
+
+                if (GetAsyncKeyState((Keys)32) < 0) {
+                    btnSpace.BackColor = Color.Red;
+                } else if (GetAsyncKeyState((Keys)32) == 0) {
+                    btnSpace.BackColor = Color.White;
+                }
+
+                if (GetAsyncKeyState((Keys)65) < 0) {
+                    btnLeft.BackColor = Color.Red;
+                } else if (GetAsyncKeyState((Keys)65) == 0) {
+                    btnLeft.BackColor = Color.White;
+                }
+
+                if (GetAsyncKeyState((Keys)68) < 0) {
+                    btnRight.BackColor = Color.Red;
+                } else if (GetAsyncKeyState((Keys)68) == 0) {
+                    btnRight.BackColor = Color.White;
+                }
+
+                if (GetAsyncKeyState(Keys.LButton | Keys.R) < 0) {
+                    btnBackward.BackColor = Color.Red;
+                } else if (GetAsyncKeyState(Keys.LButton | Keys.R) == 0) {
+                    btnBackward.BackColor = Color.White;
+                }
+
+                if (GetAsyncKeyState(Keys.LButton | Keys.V) < 0) {
+                    btnForward.BackColor = Color.Red;
+                } else if (GetAsyncKeyState(Keys.LButton | Keys.V) == 0) {
+                    btnForward.BackColor = Color.White;
+                }
+
+                if (GetAsyncKeyState((Keys)1) < 0){
+                    btnLeftClick.BackColor = Color.Red; }
+                else if (GetAsyncKeyState((Keys)1) == 0) {
+                    btnLeftClick.BackColor = Color.White;
+                }
+
+                if (GetAsyncKeyState((Keys)2) < 0) {
+                    btnRightClick.BackColor = Color.Red;
+                } else if (GetAsyncKeyState((Keys)2) == 0) {
+                    btnRightClick.BackColor = Color.White;
+                }
+
+                Thread.Sleep(frequency);
+            }
+        }
+    }
+
+}
